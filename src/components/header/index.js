097051e841d1410e9ea97605fc088bb668fc2d02
Prasanna <prasanna.krystal@gmail.com>
2019-03-12 15:36:29 +0530

    Added card component

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -2,32 +2,20 @@ import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
 
+import SynupLogo from '../../components/images/svgComponents/synupLogo';
+
+// TODO: Refactor this as per new layout
 const Header = ({ siteTitle }) => (
   <header className="navbar navbar-toggleable-sm header--fixed" id="header">
     <div className="container">
       <h1>
-        <a className="navbar-brand js-scroll-trigger" rel="home" href="index.html" title="brand-name" />
-        <svg className="desktop-view" width="96px" height="32px" viewBox="0 0 96 32" version="1.1" xmlns="http://www.w3.org/2000/svg"
-            xlinkHref="http://www.w3.org/1999/xlink">
-          <g id="Symbols" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-            <g id="UI/Topbars/Full-Light" transform="translate(-80.000000, -24.000000)" fill="#4865FF">
-              <g id="Group-2" transform="translate(72.000000, 16.000000)">
-                <g id="Group-3" transform="translate(8.000000, 8.000000)">
-                  <text id="synup" font-family="Sailec" font-size="16" font-weight="800"
-                      line-spacing="14" letter-spacing="-0.400000006">
-                    <tspan x="40" y="24">SYNU</tspan>
-                    <tspan x="85.568" y="24">P</tspan>
-                  </text>
-                  <g id="Group-7">
-                    <path d="M14.0952381,3.80952381 C21.8798231,3.80952381 28.1904762,10.1201769 28.1904762,17.9047619 C28.1904762,25.6893469 21.8798231,32 14.0952381,32 L0,32 L0,17.9047619 C-9.53336716e-16,10.1201769 6.31065305,3.80952381 14.0952381,3.80952381 Z M14.0952381,22.8571429 C16.8303626,22.8571429 19.047619,20.6398864 19.047619,17.9047619 C19.047619,15.1696374 16.8303626,12.952381 14.0952381,12.952381 C11.3601136,12.952381 9.14285714,15.1696374 9.14285714,17.9047619 C9.14285714,20.6398864 11.3601136,22.8571429 14.0952381,22.8571429 Z"
-                        id="Combined-Shape"></path>
-                    <circle id="Oval-Copy-3" cx="28.5714286" cy="3.42857143" r="3.42857143"></circle>
-                  </g>
-                </g>
-              </g>
-            </g>
-          </g>
-        </svg>
+        <a
+          className="navbar-brand js-scroll-trigger"
+          rel="home"
+          href="index.html"
+          title="brand-name"
+        />
+        <SynupLogo />
       </h1>
       <button
         className="navbar-toggler btn-nav_toggle"
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,63 @@
 import React from "react"
 
 import Layout from "../components/layout"
+import Button from '../components/atoms/Buttons';
+import Card from '../components/atoms/Card'
 
 const IndexPage = () => (
   <Layout>
-    <div>
-      Welcome to gatsby app
-    </div>
+    <main>
+      <section className="section_homepage">
+        <div className="container">
+          <div className="heroblock">
+            <h2>Get found everywhere</h2>
+            <p>Manage business location data on search, voice, and social </p>
+            <Button type="primary" text="GET STARTED" />
+          </div>
+        </div>
+      </section>
+      <section className="section_banner">
+        <div className="container">
+            <div className="banner_wrapper">
+              <h4>
+                We power location intelligence for over 
+                  <span className="primary_text">150,000</span>
+                  businesses
+              </h4>
+              <div className="banner_logoWrapper">
+                <a href="#" className="banner_logo">
+                  <img src="images/svg/v-icon.svg" className="img_fluid" alt="" />
+                </a>
+                <a href="#" className="banner_logo">
+                  <img src="images/svg/v-icon.svg" className="img_fluid" alt="" />
+                </a>
+                <a href="#" className="banner_logo">
+                  <img src="images/svg/v-icon.svg" className="img_fluid" alt="" />
+                </a>
+                <a href="#" className="banner_logo">
+                  <img src="images/svg/v-icon.svg" className="img_fluid" alt="" />
+                </a>
+                <a href="#" className="banner_logo">
+                  <img src="images/svg/v-icon.svg" className="img_fluid" alt="" />
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="section_card">
+            <div className="container">
+                <div className="card_block">
+                    <h3>What we do</h3>
+                    <p>Select your business type to get started</p>
+                </div>
+                <div className="card_deck">
+                  <Card />
+                  <Card />
+                  <Card />
+                </div>
+            </div>
+        </section>
+    </main>
   </Layout>
 )
 
