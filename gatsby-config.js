63972be5c1e05bd48c401fd342299cfc35b1108a
Prasanna <prasanna.krystal@gmail.com>
2019-03-12 09:52:38 +0530

    added access token for contentful

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,23 +18,12 @@ module.exports = {
     {
       resolve: `gatsby-source-contentful`,
       options: {
-        spaceId: `your_space_id`,
+        spaceId: `2ddwaxp8l2no`,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
+        accessToken: '0aaf089f64fd1c4f2e7e4e2a7de6df3ace0bd852b38e3e71d3e9653f19331ce7',
       },
     },
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
+
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // 'gatsby-plugin-offline',
