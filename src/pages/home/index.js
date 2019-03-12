f6aecc5809536a9e345e6daba55054463d0062d2
Prasanna <prasanna.krystal@gmail.com>
2019-03-12 16:39:28 +0530

    added feature section

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -2,10 +2,11 @@ import React from "react"
 
 import Layout from "../../components/layout"
 
-import Hero from '../../components/modules/home/Hero';
+import Hero from '../../components/modules/home/Hero'
 import Partner from '../../components/modules/home/Partner'
 import ProductWhatWeDo from '../../components/modules/home/ProductWhatWeDo'
 import MainProducts from '../../components/modules/home/MainProducts'
+import Features from '../../components/modules/home/Features'
 
 const IndexPage = () => (
   <Layout>
@@ -13,6 +14,7 @@ const IndexPage = () => (
     <Partner />
     <ProductWhatWeDo />
     <MainProducts />
+    <Features />
   </Layout>
 )
 
