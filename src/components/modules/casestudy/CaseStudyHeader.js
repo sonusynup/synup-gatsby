7ad44f1a9b569b4ea892b96c0bc316d267384ba0
Prasanna <prasanna.krystal@gmail.com>
2019-03-14 10:18:54 +0530

    Added stats and title in case study

--- src/components/modules/casestudy/CaseStudyHeader.js
+++ src/components/modules/casestudy/CaseStudyHeader.js
@@ -2,23 +2,28 @@ import React from 'react'
 
 import StatisticsCard from './StatisticsCard'
 
-const CaseStudyHeader = () => (
+const CaseStudyHeader = ({
+  title,
+  stats,
+  partner
+}) => (
   <section className="section_casestudy">
     <div className="container">
       <div className="casestudy_block">
-        <h3>How IHMC 
-          <span className="secoundary_text">increased their calls by 83%</span> using Synup</h3>
+        <h3>
+          <span className="secoundary_text">{title.casestudyTitle}</span>
+        </h3>
         <ul className="casecard_wrapper">
           <StatisticsCard
-            value="65%"
+            value={stats[0]}
             description="increase in website visits"
           />
           <StatisticsCard
-            value="61%"
+            value={stats[1]}
             description="increase in website visits"
           />
           <StatisticsCard
-            value="91%"
+            value={stats[2]}
             description="increase in website visits"
           />
         </ul>
--- src/templates/CaseStudy/index.js
+++ src/templates/CaseStudy/index.js
@@ -5,9 +5,21 @@ import Layout from '../../components/layout';
 import CaseStudyHeader from '../../components/modules/casestudy/CaseStudyHeader'
 import CaseStudyContent from '../../components/modules/casestudy/CaseStudyContent'
 
-const CaseStudy = () => (
+const CaseStudy = ({
+  pageContext: {
+    caseStudyDetails: {
+      casestudyPartner,
+      casestudyStats,
+      casestudyTitle,
+    }
+  }
+}) => (
   <Layout>
-    <CaseStudyHeader />
+    <CaseStudyHeader
+      title={casestudyTitle}
+      stats={casestudyStats}
+      partner={casestudyPartner}
+    />
     <CaseStudyContent />       
   </Layout>
 )
