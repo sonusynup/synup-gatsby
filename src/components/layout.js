e4262bd21fd0c17d54d65f295b7775466d38ebab
Prasanna <prasanna.krystal@gmail.com>
2019-03-14 18:43:59 +0530

    added footer

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,6 +11,8 @@ import Helmet from "react-helmet"
 import { StaticQuery, graphql } from "gatsby"
 
 import Header from "./header"
+import Footer from './footer'
+
 import "./layout.scss"
 
 const Layout = ({ children }) => (
@@ -41,6 +43,7 @@ const Layout = ({ children }) => (
           <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
         </Helmet>
         <main>{children}</main>
+        <Footer />
       </>
     )}
   />
