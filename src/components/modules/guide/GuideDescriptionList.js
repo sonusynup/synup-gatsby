b6f1325d04b516daf8aa10b148f2adbf943ff7da
Prasanna <prasanna.krystal@gmail.com>
2019-03-14 18:12:25 +0530

    fixed guides

--- src/components/modules/guide/GuideDescriptionList.js
+++ src/components/modules/guide/GuideDescriptionList.js
@@ -6,39 +6,11 @@ const GuideDescriptionList = ({
   image,
 }) => (
   <section>
-    <div class="container">
-      <div class="guideTemplate">
-        <div class="guideTemplate_head">
-          <p>
-            {content.content[0].content[0].value}
-          </p>
+    <div className="container">
+      <div className="guideTemplate">
+        <div className="guideTemplate_head">
+          <div dangerouslySetInnerHTML={{ __html: content.childContentfulRichText.html }} />
         </div>
-        {
-          description.guideItemDescriptionList.map((item, index) => (
-            <ul key={index} class="guideTemplate_body">
-              <li class="guideTemplate_wrapper">
-                <div class="guideTemplate_content">
-                  <span>step {index+1}</span>
-                  <h5>{item.title.title}</h5>
-                  <p>
-                    {
-                      item.description.description
-                        .split('\n')
-                        .map(line => <>{line}</>)
-                    }
-                  </p>
-                </div>
-                <div class="guideTemplate_block">
-                  <div class="guideTemplate_img">
-                    <figure>
-                      <img src={item.image.file.url} class="img_fluid" alt="" />
-                    </figure>
-                  </div>
-                </div>
-              </li>
-            </ul>
-          ))
-        }
       </div>
     </div>
   </section>
