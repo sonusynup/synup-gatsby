3d4f7852d05facbfd723b12c783723d71a1da3a6
Prasanna <prasanna.krystal@gmail.com>
2019-03-16 11:43:45 +0530

    Refactored ebook

--- src/components/modules/ebook/ebookDetails.js
+++ src/components/modules/ebook/ebookDetails.js
@@ -6,29 +6,24 @@ const EbookDetails = ({
     bookTitle,
     bookDescription,
   }
-}) => {
-  console.log(bookCoverImage, bookTitle, bookDescription)
-  return (
-    <div className="container">
-      <div className="blockWrapper">
-        <div className="ebook_block">
-          <div className="ebook_img">
-            <figure>
-              <img src={bookCoverImage.file.url} className="img_fluid" alt="" />
-            </figure>
-          </div>
-          <div className="ebook_title">
-            <h5>{bookTitle}</h5>
-          </div>
-          <div className="ebook_describtion">
-            <p className="ebook_describtion_pre">
-              {bookDescription.content[0].content[0].value}
-            </p>
-          </div>
-        </div>
-      </div>
+}) => (
+  <div className="ebook_block">
+    <div className="ebook_img">
+      <figure>
+        <img src={bookCoverImage.file.url} className="img_fluid" alt="" />
+      </figure>
     </div>
-  );
-}
+    <div className="ebook_title">
+      <h5>{bookTitle}</h5>
+    </div>
+    <div className="ebook_describtion">
+      <p className="ebook_describtion_pre">
+        {bookDescription.content[0].content[0].value}
+      </p>
+    </div>
+  </div>
+);
+
 
 export default EbookDetails;
+
