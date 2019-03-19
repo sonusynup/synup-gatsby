df8bbc82f6775d832da22b0f08d53152aa9abe8d
Prasanna <prasanna.krystal@gmail.com>
2019-03-19 11:59:26 +0530

    changed the name of the files

--- src/pages/agency/index.js
+++ src/pages/agency/index.js
@@ -2,9 +2,9 @@ import React from "react"
 
 import Layout from "../../components/layout"
 
-import HeroSub from '../../components/modules/agency/HeroSub'
-import CardGroup from '../../components/modules/agency/CardGroup'
-import CaseStudy from "../../components/modules/agency/CaseStudy";
+import HeroSub from '../../components/modules/agency/heroSub'
+import CardGroup from '../../components/modules/agency/cardGroup'
+import CaseStudy from "../../components/modules/agency/caseStudy";
 import Whitelabel from "../../components/modules/agency/whitelabel";
 
 import ActionableCardList from '../../components/molecules/ActionableCardList'
