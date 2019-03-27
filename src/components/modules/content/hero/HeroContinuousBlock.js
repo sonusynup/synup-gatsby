a14ac7a19347e346bbc1ac34ba1dddb2350c8430
Prasanna <prasanna.krystal@gmail.com>
2019-03-27 16:58:16 +0530

    added fix for block continuousg

--- src/components/modules/content/hero/HeroContinuousBlock.js
+++ src/components/modules/content/hero/HeroContinuousBlock.js
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
 import Button from '../../../atoms/button';
+import isNull from '../../../../helpers/isNull';
 
 // Renders Hero Component
 const HeroContinuousBlock = ({
@@ -13,25 +14,39 @@ const HeroContinuousBlock = ({
   heroDescription,
   heroButtonLink,
   heroBackground,
+  metricValue,
+  metricText,
 }) => (
-  <section class="section_hero paddingControl">
-    <div class="container">
-      <div class="heroblock">
-        <img class="heroblock_icon" src={heroIcon.file.url} alt="" />
-        <h2>{heroTitle}</h2>
-        <p dangerouslySetInnerHTML={{ __html: heroDescription.childContentfulRichText.html }} />
-        <Link to={''}>
-          <Button 
-            type="primary"
-            text={heroButtonText}    
-          />
-        </Link>
+  <div className="hero-continuous-block">
+    <section class="section_hero paddingControl">
+      <div class="container">
+        <div class="heroblock">
+          <img class="heroblock_icon" src={heroIcon.file.url} alt="" />
+          <h2>{heroTitle}</h2>
+          <p dangerouslySetInnerHTML={{ __html: heroDescription.childContentfulRichText.html }} />
+          <Link to={''}>
+            <Button 
+              type="primary"
+              text={heroButtonText}    
+            />
+          </Link>
+        </div>
+        <div class="heroblockImg">
+          <figure><img src={heroBackground.file.url} class="img_fluid" alt="" /></figure>
+        </div>
       </div>
-      <div class="heroblockImg">
-        <figure><img src={heroBackground.file.url} class="img_fluid" alt="" /></figure>
-      </div>
-    </div>
-  </section>
+    </section>
+    {
+      (!isNull(metricValue) && !isNull(metricText)) && (
+        <div class="container">
+          <div class="labelBlock bgBlue">
+            <h3 class="labelTitle">{metricValue}</h3>
+            <p class="labelDescription">{metricText}</p>
+          </div>
+        </div>
+      )
+    }
+  </div>
 )
 
 HeroContinuousBlock.propTypes = {
