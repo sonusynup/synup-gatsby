f8d5f7ba69b3bcfa3b9ad9be1fd2e8e2812993c4
Prasanna <prasanna.krystal@gmail.com>
2019-03-28 11:25:14 +0530

    default case study refactored

--- src/components/modules/content/caseStudy/testimonial/default.js
+++ src/components/modules/content/caseStudy/testimonial/default.js
@@ -1,6 +1,9 @@
 import React from "react"
 
-const Type1 = ({
+import PropTypes from 'prop-types'
+import isNull from '../../../../../helpers/isNull'
+
+const DefaultTestimonial = ({
   partnerPhoto,
   partnerTestimonial,
   partnerName,
@@ -13,21 +16,36 @@ const Type1 = ({
         <div className="overlay_content">
           <div className="testimonial_overlay green-overlay" />
           <figure>
-            <img src={partnerPhoto.file.url} className="img_fluid" alt="" />
+            {
+              !isNull(partnerPhoto) ? (
+                <img
+                  src={partnerPhoto.file.url}
+                  className="img_fluid"
+                  alt=""
+                />
+              ) : null
+            }
           </figure>
         </div>
       </div>
-      <div
-        className="testimonialContentOutline"
-        dangerouslySetInnerHTML={{ __html: partnerTestimonial.childContentfulRichText.html }}
-      />
+      {
+        !isNull(partnerTestimonial) ? (
+          <div
+            className="testimonialContentOutline"
+            dangerouslySetInnerHTML={{ __html: partnerTestimonial.childContentfulRichText.html }}
+          />
+        ) : null
+      }
       <div className="testimonialAuthor">
         <div className="testimonialAuthor_details">
-          <h6 className="testimonialAuthor_name">{partnerName}</h6>
-          <p
-            className="testimonialAuthor_position"
-            dangerouslySetInnerHTML={{ __html: partnerDescription.childContentfulRichText.html }}
-          />
+          {!isNull(partnerName) ? <h6 className="testimonialAuthor_name">{partnerName}</h6> : null}
+          {!isNull(partnerDescription) ? (
+            <p
+              className="testimonialAuthor_position"
+              dangerouslySetInnerHTML={{ __html: partnerDescription.childContentfulRichText.html }}
+            />
+            ) : null
+          }
         </div>
       </div>
     </div>
@@ -35,4 +53,44 @@ const Type1 = ({
   </div>
 )
 
-export default Type1
+DefaultTestimonial.propTypes = {
+  partnerPhoto: PropTypes.shape({
+    file: PropTypes.shape({
+      url: PropTypes.string,
+    })
+  }),
+  partnerTestimonial: PropTypes.shape({
+    childContentfulRichText: PropTypes.shape({
+      html: PropTypes.string,
+    })
+  }),
+  partnerName: PropTypes.string,
+  partnerDescription: PropTypes.shape({
+    childContentfulRichText: PropTypes.shape({
+      html: PropTypes.string,
+    })
+  }),
+  children: PropTypes.node,
+}
+
+DefaultTestimonial.defaultProps = {
+  partnerPhoto: {
+    file: {
+      url: PropTypes.string,
+    },
+  },
+  partnerTestimonial: {
+    childContentfulRichText: {
+      html: PropTypes.string,
+    }
+  },
+  partnerName: PropTypes.string,
+  partnerDescription: {
+    childContentfulRichText: {
+      html: PropTypes.string,
+    }
+  },
+  children: PropTypes.node,
+}
+
+export default DefaultTestimonial
