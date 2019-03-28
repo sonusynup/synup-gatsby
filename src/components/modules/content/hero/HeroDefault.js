5935fb2acf11ddd68cd436464bad8c55b8d2be23
Prasanna <prasanna.krystal@gmail.com>
2019-03-28 09:44:58 +0530

    Added hero default based on existence

--- src/components/modules/content/hero/HeroDefault.js
+++ src/components/modules/content/hero/HeroDefault.js
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types'
 import { Link } from 'gatsby'
 
 import Button from '../../../atoms/button';
+import isNull from '../../../../helpers/isNull';
 
-// Renders Hero Component
+// Renders de
 const HeroDefault = ({
   heroTitle,
   heroButtonText,
@@ -16,11 +17,19 @@ const HeroDefault = ({
   <section className="section_homepage" style={{ backgroundImage: `url(${heroBackground.file.url})` }}>
     <div className="container">
       <div className="heroblock">
-        <h2>{heroTitle}</h2>
-        <p dangerouslySetInnerHTML={{ __html: heroDescription.childContentfulRichText.html }}/>
-        <Link to={heroButtonLink}>
-          <Button type="primary" text={heroButtonText}/>
-        </Link>
+        {heroTitle ? <h2>{heroTitle}</h2> : null}
+        {
+          heroDescription ? (
+            <p dangerouslySetInnerHTML={{ __html: heroDescription.childContentfulRichText.html }} />
+          ) : null
+        }
+        {
+          (!isNull(heroButtonText) && !isNull(heroButtonLink)) ? (
+            <Link to={heroButtonLink}>
+              <Button type="primary" text={heroButtonText}/>
+            </Link>
+          ) : null
+        }
       </div>
     </div>
   </section>
@@ -31,6 +40,15 @@ HeroDefault.propTypes = {
   heroButtonText: PropTypes.string,
   heroButtonLink: PropTypes.string,
   heroBackground: PropTypes.string,
+  heroBackground: PropTypes.string,
+}
+
+HeroDefault.defaultProps = {
+  heroTitle: null,
+  heroButtonText: null,
+  heroButtonLink: null,
+  heroBackground: null,
+  heroBackground: null,
 }
 
 export default HeroDefault
