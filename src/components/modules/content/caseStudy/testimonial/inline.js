5bfbb2bd21a459be89600ed7dcae63b29a07420f
Prasanna <prasanna.krystal@gmail.com>
2019-03-28 12:01:10 +0530

    Added fix for case study inline

--- src/components/modules/content/caseStudy/testimonial/inline.js
+++ src/components/modules/content/caseStudy/testimonial/inline.js
@@ -1,29 +1,31 @@
 import React from "react"
 
-const Type2 = ({
-  partnerImage,
+import PropTypes from 'prop-types'
+
+const InlineTestimonial = ({
+  partnerPhoto,
   partnerTestimonial,
   partnerName,
-  partnerPosition,
+  partnerDescription,
   children,
 }) => (
   <div className="blockWrapper">
     <div class="testimonialWrapper">
       <p
         class="testimonialContent"
-        dangerouslySetInnerHTML={{ __html: partnerTestimonial }}
+        dangerouslySetInnerHTML={{ __html: partnerTestimonial.childContentfulRichText.html }}
       />
       <div class="testimonialAuthor">
         <div class="testimonialAuthor_img">
           <figure>
-            <img src={partnerImage} class="avartar-img" alt="" />
+            <img src={partnerPhoto.file.url} class="avartar-img" alt="" />
           </figure>
         </div>
         <div class="testimonialAuthor_details">
           <h6 class="testimonialAuthor_name">{partnerName}</h6>
           <p
             class="testimonialAuthor_position"
-            dangerouslySetInnerHTML={{ __html: partnerPosition }}
+            dangerouslySetInnerHTML={{ __html: partnerDescription.childContentfulRichText.html }}
           />
         </div>
       </div>
@@ -32,4 +34,44 @@ const Type2 = ({
   </div>
 )
 
-export default Type2
+InlineTestimonial.propTypes = {
+  partnerPhoto: PropTypes.shape({
+    file: PropTypes.shape({
+      url: PropTypes.string,
+    })
+  }),
+  partnerTestimonial: PropTypes.shape({
+    childContentfulRichText: PropTypes.shape({
+      html: PropTypes.string,
+    })
+  }),
+  partnerName: PropTypes.string,
+  partnerDescription: PropTypes.shape({
+    childContentfulRichText: PropTypes.shape({
+      html: PropTypes.string,
+    })
+  }),
+  children: PropTypes.node,
+}
+
+InlineTestimonial.defaultProps = {
+  partnerPhoto: {
+    file: {
+      url: PropTypes.string,
+    },
+  },
+  partnerTestimonial: {
+    childContentfulRichText: {
+      html: PropTypes.string,
+    }
+  },
+  partnerName: PropTypes.string,
+  partnerDescription: {
+    childContentfulRichText: {
+      html: PropTypes.string,
+    }
+  },
+  children: PropTypes.node,
+}
+
+export default InlineTestimonial
