f827c0e3d609d3d36f3914662c367ac569772190
Prasanna <prasanna.krystal@gmail.com>
2019-04-10 11:41:28 +0530

    Added timing format

--- src/components/modules/webinar/webinarDetails.js
+++ src/components/modules/webinar/webinarDetails.js
@@ -3,9 +3,20 @@ import WebinarSpeakers from "./webinarSpeakers"
 
 // Todo bring this to the right format
 // January 15, 2019 / 9:00AM EST</pre>
-// const parseTime = date => {
-//   return date
-// }
+const parseTime = time => {
+  const date = new Date(time);
+  const year = date.getFullYear();
+  let month = date.getMonth();
+  let dt = date.getDate();
+  if (dt < 10) {
+    dt = '0' + dt;
+  }
+  if (month < 10) {
+    month = '0' + month;
+  }
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return `${months[month-'0']} ${dt} / ${time.split('T')[1]} EST`;
+}
 
 // Renders the webinar details
 const WebinarDetails = ({
@@ -21,7 +32,7 @@ const WebinarDetails = ({
       <span className="badge badge_secondary">{status} Webinars</span>
       <h3>{title}</h3>
       <div className="date_wrapper webinar_description">
-        <pre>{date}</pre>
+        <pre>{parseTime(date)}</pre>
         <pre>Duration: {duration} mins</pre>
       </div>
       <p className="webinar_description">{description.webinarDescription}</p>
